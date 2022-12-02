b1cdc50d9f149f6fc60cbacd91fe2b38854677f2
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2022-12-02 19:32:09 +0100

    dom

---
+++ js/dom.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let caja = document.getElementById('micaja');
+
+// let caja = document.getElementById('micaja').innerHTML;
+
+caja.innerHTML = "ahora me llamo diferente";
+console.log(caja);
+caja.style.background = 'red';
