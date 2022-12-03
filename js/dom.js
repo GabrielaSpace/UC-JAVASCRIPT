fabd106327914d955f3d2cfb213a90b97a70a5fb
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2022-12-03 01:54:18 +0100

    etiquetas

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,26 @@
 'use strict'
 
-let caja = document.getElementById('micaja');
+// let caja = document.getElementById('micaja');
+    let caja = document.querySelector('#micaja');
 
 // let caja = document.getElementById('micaja').innerHTML;
 
 caja.innerHTML = "ahora me llamo diferente";
 console.log(caja);
 caja.style.background = 'red';
+caja.style.padding="20px";
+caja.style.color='white';
+
+function cambiaColor(color) {
+    caja.style.background = color;   
+}
+caja.className ='hola';
+//conseguir elemntos por su etiqueta
+
+let todosLosDivs = document.getElementsByTagName('div');
+console.log(todosLosDivs);
+
+// let contenidoEnTexto = todosLosDivs[2].textContent;
+let contenidoEnTexto = todosLosDivs[2];
+contenidoEnTexto.innerHTML = "soy otro texto para probar";
+console.log(contenidoEnTexto);
